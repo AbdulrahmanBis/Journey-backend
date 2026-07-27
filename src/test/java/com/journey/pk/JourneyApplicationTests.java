9b7168efe2167e58329d89f0663002c028afaec3
AbdulrhmanBis <AbdulrhmanBis@hotmail.com>
2026-07-27 14:09:36 +0300

    first commit

---
+++ src/test/java/com/journey/pk/JourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journey.pk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
